98d27226dc0f755e265ec1ee0d2770657aadbc2d
Molizo <visoiu.mihnea@live.com>
2018-07-05 16:37:56 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int x;
+    cout<<endl<<"Introduceti numarul:";
+    cin>>x;
+    if(x%2==0)
+        cout<<"Numarul este par.";
+    else
+        cout<<"Numarul este impar.";
+    return 0;
+}
